52a3c8968882b339b73d553162e47c3a0fa45703
Aldrin Tingson <aldrin.tingson@mecca.com.au>
2024-03-19 16:07:08 +0800

    removing custom font

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Link, Tabs } from "expo-router";
+import { Link } from "expo-router";
 import React from "react";
 import { Pressable } from "react-native";
 
