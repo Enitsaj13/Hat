9f4c07fc65de9d58f0d083dcaccbc38e6980db1b
Jastine Formentera <enitsajformentera.13@gmail.com>
2024-04-16 19:54:42 +0800

    Updated codes for record screen

--- src/app/(app)/(tabs)/(one)/Record.tsx
+++ src/app/(app)/(tabs)/(one)/Record.tsx
@@ -1,30 +1,91 @@
 import DropdownList from "@components/DropdownList";
 import { AntDesign as Icon } from "@expo/vector-icons";
-import { i18n } from "@i18n/index";
-import {
-  ExtractedObservables,
-  withObservables,
-} from "@nozbe/watermelondb/react";
-import { database } from "@stores/index";
-import { useState } from "react";
-import { Keyboard, TouchableWithoutFeedback, View } from "react-native";
-import { Button, Text, TextInput } from "react-native-paper";
-import { createStyleSheet, useStyles } from "react-native-unistyles";
-import { AuditType } from "@stores/auditType";
-import isEmpty from "lodash.isempty";
-import { CompanyConfig } from "@stores/companyConfig";
+import { getDefaultLanguage, i18n } from "@i18n/index";
 import { Q } from "@nozbe/watermelondb";
+import { withObservables } from "@nozbe/watermelondb/react";
+import { ObservableifyProps } from "@nozbe/watermelondb/react/withObservables";
+import { AppSetting } from "@stores/appSetting";
+import { database } from "@stores/index";
+import { colors } from "@theme/index";
+import { Link } from "expo-router";
+import isEmpty from "lodash.isempty";
+import { useCallback, useState } from "react";
+import { View, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { Button, TextInput, Text } from "react-native-paper";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
 import { of, switchMap } from "rxjs";
+interface RecordProps {
+  appSetting: AppSetting;
+}
 
-function Component({ auditTypes, companyConfig }: RecordProps) {
+export const typeAudit = [
+  { key: "select", value: "*Select Audit Type" },
+  { key: "endemic", value: "Endemic" },
+  { key: "pandemic", value: "Pandemic" },
+  { key: "covid19", value: "Covid 19" },
+  { key: "normal", value: "Normal" },
+];
+
+function Component({ appSetting }: RecordProps) {
+  const saveSelectedLanguage = useCallback(
+    async (languageCode: string) => {
+      if (isEmpty(appSetting)) {
+        await database.write(async () => {
+          await database
+            .get<AppSetting>("app_settings")
+            .create((appSetting) => {
+              appSetting.language = languageCode;
+              appSetting.dataPrivacyUrl = "";
+              appSetting.termsOfUseUrl = "";
+            });
+        });
+      } else {
+        await appSetting.saveLanguage(languageCode);
+      }
+    },
+    [appSetting],
+  );
   const { styles } = useStyles(stylesheet);
 
   const [numberOpportunities, setNumberOpportunities] = useState("");
-  const [auditType, setAuditType] = useState<number | undefined>();
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
+        <View style={styles.recordContainer}>
+          <View style={styles.rowContainer}>
+            <Text variant="bodyLarge" style={styles.recordText}>
+              Target Opportunies
+            </Text>
+            <Text variant="bodyLarge" style={styles.recordText}>
+              50
+            </Text>
+          </View>
+          <View style={styles.rowContainer}>
+            <Text variant="bodyLarge" style={styles.recordText}>
+              Balance Remaining
+            </Text>
+            <Text variant="bodyLarge" style={styles.recordText}>
+              50
+            </Text>
+          </View>
+          <View style={styles.rowContainer}>
+            <Text variant="bodyLarge" style={styles.recordText}>
+              Complete Target By
+            </Text>
+            <Text variant="bodyLarge" style={styles.recordText}>
+              March 31, 2023
+            </Text>
+          </View>
+          <View style={styles.rowContainer}>
+            <Text variant="bodyLarge" style={styles.recordText}>
+              Last Submission On
+            </Text>
+            <Text variant="bodyLarge" style={styles.recordText}>
+              50
+            </Text>
+          </View>
+        </View>
         <View style={styles.textInputContainer}>
           <TextInput
             keyboardType="numeric"
@@ -34,21 +95,23 @@ function Component({ auditTypes, companyConfig }: RecordProps) {
             value={numberOpportunities}
             onChangeText={(text) => setNumberOpportunities(text)}
             contentStyle={styles.textInput}
-            theme={{ colors: { primary: "#475569" } }}
-            textColor="#020617"
+            theme={{
+              colors: {
+                placeholder: colors.cadetGrey,
+                primary: colors.cadetGrey,
+              },
+            }}
+            underlineColor="white"
+            underlineColorAndroid={colors.cadetGrey}
+            textColor="black"
           />
-          {isEmpty(companyConfig) || companyConfig.enableAuditTypes ? (
-            <DropdownList
-              options={auditTypes.map((a) => ({
-                key: a.serverId,
-                value: a.name,
-              }))}
-              onOptionSelected={(key) => setAuditType(key as number)}
-              selectedOptionKey={auditType}
-              dropdownlistStyle={styles.dropdownlistContainer}
-              right={<Icon name="caretdown" size={12} color="gray" />}
-            />
-          ) : null}
+          <DropdownList
+            options={typeAudit}
+            selectedOptionKey={getDefaultLanguage(appSetting)}
+            onOptionSelected={(key) => saveSelectedLanguage(key as string)}
+            dropdownlistStyle={styles.dropdownlistContainer}
+            right={<Icon name="caretdown" size={12} color="gray" />}
+          />
           <Button
             mode="outlined"
             style={styles.recordButton}
@@ -62,10 +125,38 @@ function Component({ auditTypes, companyConfig }: RecordProps) {
             <Icon name="arrowright" size={14} color="white" />
           </Button>
         </View>
+        <View style={styles.practiceButtonContainer}>
+          <Link href="/PracticeMode" asChild>
+            <Button
+              mode="text"
+              onPress={() => {
+                console.log("Clicked!");
+              }}
+            >
+              <Text style={styles.practiceButton}>Try on Practice Mode</Text>
+            </Button>
+          </Link>
+        </View>
       </View>
     </TouchableWithoutFeedback>
   );
 }
+
+type WithObservableProps = ObservableifyProps<RecordProps, "appSetting">;
+const Record = withObservables(
+  ["appSetting"],
+  (props: WithObservableProps) => ({
+    appSetting: database
+      .get<AppSetting>("app_settings")
+      .query(Q.take(1))
+      .observe()
+      .pipe(
+        switchMap((appSettings) =>
+          appSettings.length > 0 ? appSettings[0].observe() : of(null),
+        ),
+      ),
+  }),
+)(Component as any);
 
 const stylesheet = createStyleSheet({
   container: {
@@ -75,19 +166,18 @@ const stylesheet = createStyleSheet({
   },
   textInputContainer: {
     width: "95%",
-    marginTop: 40,
   },
   textInput: {
-    height: 80,
     backgroundColor: "white",
-    borderWidth: 0.2,
+    borderWidth: 0.4,
+    borderColor: colors.cadetGrey,
+    height: 80,
     borderRadius: 2,
-    borderColor: "#94a3b8",
   },
   dropdownlistContainer: {
-    borderWidth: 0.2,
+    borderWidth: 0.4,
     borderRadius: 0,
-    borderColor: "gray",
+    borderColor: colors.cadetGrey,
     marginTop: 20,
     flexDirection: "row",
     alignItems: "center",
@@ -96,38 +186,37 @@ const stylesheet = createStyleSheet({
     padding: 14,
   },
   recordButton: {
-    backgroundColor: "#01b482",
+    backgroundColor: colors.bgColor,
     marginTop: 20,
     borderWidth: 0,
     borderRadius: 4,
     padding: 4,
   },
+  recordContainer: {
+    gap: 10,
+    width: "96%",
+    marginVertical: 20,
+    borderWidth: 2,
+    padding: 10,
+    borderColor: "purple",
+  },
+  rowContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  recordText: {
+    color: "black",
+  },
+  practiceButtonContainer: {
+    position: "absolute",
+    bottom: 0,
+  },
+  practiceButton: {
+    color: "black",
+    fontSize: 18,
+    textDecorationLine: "underline",
+    textDecorationColor: "purple",
+  },
 });
-
-interface ObservableProps {
-  auditTypes: AuditType[];
-  companyConfig?: CompanyConfig | undefined;
-}
-
-const getObservables = (props: ObservableProps) => ({
-  auditTypes: database.get<AuditType>("audit_types").query(),
-  companyConfig: database
-    .get<CompanyConfig>("company_configs")
-    .query(Q.take(1))
-    .observe()
-    .pipe(
-      switchMap((companyConfig) =>
-        companyConfig.length > 0 ? companyConfig[0].observe() : of(null),
-      ),
-    ),
-});
-
-interface RecordProps
-  extends ExtractedObservables<ReturnType<typeof getObservables>> {}
-
-const Record = withObservables(
-  ["auditTypes", "companyConfig"],
-  getObservables,
-)(Component);
 
 export default Record;
