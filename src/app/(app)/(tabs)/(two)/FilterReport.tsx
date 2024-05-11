35c76453c669f6b29cdc83598f4688a9070d8d56
Jastine Formentera <enitsajformentera.13@gmail.com>
2024-05-11 10:12:35 +0800

    remove unused component and change the font icon name

--- src/app/(app)/(tabs)/(two)/FilterReport.tsx
+++ src/app/(app)/(tabs)/(two)/FilterReport.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Button } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { createStyleSheet } from "react-native-unistyles";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
 import DropdownList from "@components/DropdownList";
 import {
-  AntDesign as Icon,
-  FontAwesome as RepeatIcon,
+    AntDesign as Icon,
+    FontAwesome as FontAwesomeIcon,
 } from "@expo/vector-icons";
 import { colors } from "@theme/index";
 import { i18n } from "@i18n/index";
@@ -13,219 +12,215 @@ import { router } from "expo-router";
 import CustomDatePicker from "@components/CustomDatePicker";
 
 const AuditorType = [
-  { key: "all", value: "--All--" },
-  { key: "jastine", value: "Jastine" },
-  { key: "aldrin", value: "Aldrin" },
-  { key: "ronald", value: "Ronald" },
-  { key: "aljon", value: "Aljon" },
+    { key: "all", value: "--All--" },
+    { key: "jastine", value: "Jastine" },
+    { key: "aldrin", value: "Aldrin" },
+    { key: "ronald", value: "Ronald" },
+    { key: "aljon", value: "Aljon" },
 ];
 
 const FilterReport = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(new Date());
 
-  const handleDateChange = (date: Date) => {
-    setSelectedDate(date);
-  };
+    const handleDateChange = (date: Date) => {
+        setSelectedDate(date);
+    };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.borderContainer}>
-        <View style={styles.selectorContainer}>
-          <Text style={styles.text}>
-            {i18n.t("T2", { defaultValue: "Date From:" })}:
-          </Text>
-          <CustomDatePicker
-            initialDate={selectedDate}
-            onDateChange={handleDateChange}
-          />
-        </View>
-        <View style={styles.selectorContainer}>
-          <Text style={styles.text}>
-            {i18n.t("T3", { defaultValue: "Date To:" })}:
-          </Text>
-          <CustomDatePicker
-            initialDate={selectedDate}
-            onDateChange={handleDateChange}
-          />
-        </View>
-        <View style={styles.selectorContainer}>
-          <Text style={styles.text}>
-            {i18n.t("T4", { defaultValue: "Auditor:" })}:
-          </Text>
-          <DropdownList
-            options={AuditorType}
-            dropdownlistStyle={styles.dropdownlistContainer}
-            selectedValueStyle={styles.selectedValue}
-            right={
-              <Icon
-                name="caretdown"
-                size={10}
-                style={styles.arrowIcon}
-                color="gray"
-              />
-            }
-            noOptionSelectedText="--All--"
-          />
-        </View>
-        <View style={styles.selectorContainer}>
-          <Text style={styles.text}>
-            {i18n.t("T5", { defaultValue: "HCW:" })}:
-          </Text>
-          <DropdownList
-            options={AuditorType}
-            dropdownlistStyle={styles.dropdownlistContainer}
-            selectedValueStyle={styles.selectedValue}
-            right={
-              <Icon
-                name="caretdown"
-                size={10}
-                style={styles.arrowIcon}
-                color="gray"
-              />
-            }
-            noOptionSelectedText="--All--"
-          />
-        </View>
-        <View style={[styles.selectorContainer, styles.locationHeader]}>
-          <Text style={styles.text}>
-            {i18n.t("LC1", { defaultValue: "Location" })}:
-          </Text>
-        </View>
+    return (
+        <View style={styles.container}>
+            <View style={styles.borderContainer}>
+                <View style={styles.selectorContainer}>
+                    <Text style={styles.text}>
+                        {i18n.t("T2", { defaultValue: "Date From:" })}:
+                    </Text>
+                    <CustomDatePicker initialDate={selectedDate} onDateChange={handleDateChange} />
+                </View>
+                <View style={styles.selectorContainer}>
+                    <Text style={styles.text}>
+                        {i18n.t("T3", { defaultValue: "Date To:" })}:
+                    </Text>
+                    <CustomDatePicker initialDate={selectedDate} onDateChange={handleDateChange} />
+                </View>
+                <View style={styles.selectorContainer}>
+                    <Text style={styles.text}>
+                        {i18n.t("T4", { defaultValue: "Auditor:" })}:
+                    </Text>
+                    <DropdownList
+                        options={AuditorType}
+                        dropdownlistStyle={styles.dropdownlistContainer}
+                        selectedValueStyle={styles.selectedValue}
+                        right={
+                            <Icon
+                                name="caretdown"
+                                size={10}
+                                style={styles.arrowIcon}
+                                color="gray"
+                            />
+                        }
+                        noOptionSelectedText="--All--"
+                    />
+                </View>
+                <View style={styles.selectorContainer}>
+                    <Text style={styles.text}>
+                        {i18n.t("T5", { defaultValue: "HCW:" })}:
+                    </Text>
+                    <DropdownList
+                        options={AuditorType}
+                        dropdownlistStyle={styles.dropdownlistContainer}
+                        selectedValueStyle={styles.selectedValue}
+                        right={
+                            <Icon
+                                name="caretdown"
+                                size={10}
+                                style={styles.arrowIcon}
+                                color="gray"
+                            />
+                        }
+                        noOptionSelectedText="--All--"
+                    />
+                </View>
+                <View style={[styles.selectorContainer, styles.locationHeader]}>
+                    <Text style={styles.text}>
+                        {i18n.t("LC1", { defaultValue: "Location" })}:
+                    </Text>
+                </View>
 
-        <View style={[styles.selectorContainer, styles.locationContainer]}>
-          <Text style={styles.text}>-{i18n.t("NO_LOCATION_SELECTED")}-</Text>
-          <TouchableOpacity style={styles.repeatIcon}>
-            <RepeatIcon name="repeat" size={24} color="black" />
-          </TouchableOpacity>
+                <View style={[styles.selectorContainer, styles.locationContainer]}>
+                    <Text style={styles.text}>
+                        -{i18n.t("NO_LOCATION_SELECTED")}-
+                    </Text>
+                    <TouchableOpacity style={styles.repeatIcon}>
+                        <FontAwesomeIcon name="repeat" size={24} color="black" />
+                    </TouchableOpacity>
+                </View>
+                <View style={[styles.selectorContainer, styles.dropdownlistLocation]}>
+                    <Text style={styles.text}>
+                        {i18n.t("LC2", { defaultValue: "Location:" })}:
+                    </Text>
+                    <DropdownList
+                        options={AuditorType}
+                        dropdownlistStyle={styles.dropdownlistContainer}
+                        selectedValueStyle={styles.selectedValue}
+                        right={
+                            <Icon
+                                name="caretdown"
+                                size={10}
+                                style={styles.arrowIcon}
+                                color="gray"
+                            />
+                        }
+                        noOptionSelectedText="Select"
+                    />
+                </View>
+            </View>
+            <View style={styles.filterButtonContainer}>
+                <TouchableOpacity
+                    style={styles.cancelButton}
+                    onPress={() => router.back()}
+                >
+                    <Text style={styles.cancelTitle}>
+                        {i18n.t("T10", { defaultValue: "Cancel" })}
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.filterButton}>
+                    <Text style={styles.filterTitle}>
+                        {i18n.t("S4", { defaultValue: "Filter" })}
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
-        <View style={[styles.selectorContainer, styles.dropdownlistLocation]}>
-          <Text style={styles.text}>
-            {i18n.t("LC2", { defaultValue: "Location:" })}:
-          </Text>
-          <DropdownList
-            options={AuditorType}
-            dropdownlistStyle={styles.dropdownlistContainer}
-            selectedValueStyle={styles.selectedValue}
-            right={
-              <Icon
-                name="caretdown"
-                size={10}
-                style={styles.arrowIcon}
-                color="gray"
-              />
-            }
-            noOptionSelectedText="Select"
-          />
-        </View>
-      </View>
-      <View style={styles.filterButtonContainer}>
-        <TouchableOpacity
-          style={styles.cancelButton}
-          onPress={() => router.back()}
-        >
-          <Text style={styles.cancelTitle}>
-            {i18n.t("T10", { defaultValue: "Cancel" })}
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.filterButton}>
-          <Text style={styles.filterTitle}>
-            {i18n.t("S4", { defaultValue: "Filter" })}
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    );
 };
 
 export default FilterReport;
 
 const styles = createStyleSheet({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    padding: 20,
-  },
-  borderContainer: {
-    borderWidth: 0.4,
-    borderColor: colors.steelGrey,
-    height: "72%",
-  },
-  selectorContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    borderBottomWidth: 0.4,
-    borderBottomColor: colors.steelGrey,
-    paddingHorizontal: 12,
-    height: 50,
-  },
-  text: {
-    color: colors.steelGrey,
-    fontSize: 16,
-  },
-  dropdownContainer: {
-    paddingHorizontal: 0,
-  },
-  dropdownlistContainer: {
-    backgroundColor: colors.textColor,
-  },
-  selectedValue: {
-    fontSize: 14,
-    color: colors.steelGrey,
-  },
-  arrowIcon: {
-    marginLeft: 15,
-  },
-  locationHeader: {
-    backgroundColor: "#fafafa",
-  },
-  locationContainer: {
-    marginTop: 10,
-    borderBottomWidth: 0,
-  },
-  repeatIcon: {
-    marginRight: 5,
-  },
-  dropdownlistLocation: {
-    borderWidth: 0.4,
-    borderColor: colors.steelGrey,
-    marginHorizontal: 15,
-    height: 42,
-  },
-  filterButtonContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-  },
-  cancelButton: {
-    backgroundColor: "#fafafa",
-    width: 168,
-    height: 60,
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: colors.midNight,
-    shadowOffset: {
-      width: 1,
-      height: -0.5,
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+        padding: 20,
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 1,
-    elevation: 5,
-  },
-  cancelTitle: {
-    color: colors.steelGrey,
-    fontSize: 16,
-  },
-  filterButton: {
-    width: 168,
-    height: 60,
-    backgroundColor: colors.bgColor,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  filterTitle: {
-    color: colors.textColor,
-    fontSize: 16,
-  },
+    borderContainer: {
+        borderWidth: 0.4,
+        borderColor: colors.steelGrey,
+        height: "72%",
+    },
+    selectorContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderBottomWidth: 0.4,
+        borderBottomColor: colors.steelGrey,
+        paddingHorizontal: 12,
+        height: 50,
+    },
+    text: {
+        color: colors.steelGrey,
+        fontSize: 16,
+    },
+    dropdownContainer: {
+        paddingHorizontal: 0,
+    },
+    dropdownlistContainer: {
+        backgroundColor: colors.textColor,
+    },
+    selectedValue: {
+        fontSize: 14,
+        color: colors.steelGrey,
+    },
+    arrowIcon: {
+        marginLeft: 15,
+    },
+    locationHeader: {
+        backgroundColor: "#fafafa",
+    },
+    locationContainer: {
+        marginTop: 10,
+        borderBottomWidth: 0,
+    },
+    repeatIcon: {
+        marginRight: 5,
+    },
+    dropdownlistLocation: {
+        borderWidth: 0.4,
+        borderColor: colors.steelGrey,
+        marginHorizontal: 15,
+        height: 42,
+    },
+    filterButtonContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+    },
+    cancelButton: {
+        backgroundColor: "#fafafa",
+        width: 168,
+        height: 60,
+        justifyContent: "center",
+        alignItems: "center",
+        shadowColor: colors.midNight,
+        shadowOffset: {
+            width: 1,
+            height: -0.5,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 1,
+        elevation: 5,
+    },
+    cancelTitle: {
+        color: colors.steelGrey,
+        fontSize: 16,
+    },
+    filterButton: {
+        width: 168,
+        height: 60,
+        backgroundColor: colors.bgColor,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    filterTitle: {
+        color: colors.textColor,
+        fontSize: 16,
+    },
 });
--- src/app/(app)/(tabs)/(two)/Reports.tsx
+++ src/app/(app)/(tabs)/(two)/Reports.tsx
@@ -7,144 +7,144 @@ import { i18n, i18nOptions } from "@i18n/index";
 const currentDate: Date = new Date();
 
 const formattedDate: string = currentDate.toLocaleString(
-  "en-US",
-  i18nOptions.dateFormatOptions,
+    "en-US",
+    i18nOptions.dateFormatOptions,
 );
 
 const Reports = () => {
-  return (
-    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-      <View style={[styles.rowContainer, styles.titleContainer]}>
-        <Text style={styles.title}>
-          {i18n.t("U3", { defaultValue: "Full Details" })}
-        </Text>
-      </View>
-      <View style={[styles.rowContainer, styles.rowGray]}>
-        <Text style={styles.text}>
-          {i18n.t("U4", { defaultValue: "Location Audited" })}
-        </Text>
-      </View>
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("B6", { defaultValue: "Department 4" })} 4
-        </Text>
-      </View>
-      <View style={styles.grayEmpty} />
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("U10", { defaultValue: "Date and Time" })} 4
-        </Text>
-        <Text style={styles.grayText}>{formattedDate}</Text>
-      </View>
-      <View style={styles.grayEmpty} />
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("U11", { defaultValue: "Type of HCW 4" })}
-        </Text>
-        <Text style={styles.grayText}>Nurse</Text>
-      </View>
-      <View style={styles.grayEmpty} />
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("ADD2", { defaultValue: "Moments" })}
-        </Text>
-        <Text style={styles.grayText}>1 2 3</Text>
-      </View>
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("U14", { defaultValue: "H.H. Action" })}
-        </Text>
-        <Text style={styles.grayText}>
-          {i18n.t("AE10", { defaultValue: "Wash" })}
-        </Text>
-      </View>
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("U15", { defaultValue: "H.H. Result" })}
-        </Text>
-        <Text style={styles.grayText}>
-          {i18n.t("S10", { defaultValue: "Passed" })}
-        </Text>
-      </View>
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("U16", { defaultValue: "Occupational Risk" })}
-        </Text>
-        <Text style={styles.grayText}>-</Text>
-      </View>
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("U17", { defaultValue: "Glove Compliance" })}
-        </Text>
-        <Text style={styles.grayText}>-</Text>
-      </View>
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("U18", { defaultValue: "Gown Compliance" })}
-        </Text>
-        <Text style={styles.grayText}>-</Text>
-      </View>
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("U19", { defaultValue: "Mask Compliance" })}
-        </Text>
-        <Text style={styles.grayText}>-</Text>
-      </View>
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("U20", { defaultValue: "Mask Type" })}
-        </Text>
-        <Text style={styles.grayText}>-</Text>
-      </View>
-      <View style={styles.rowContainer}>
-        <Text style={styles.text}>
-          {i18n.t("U21", { defaultValue: "Notes" })}
-        </Text>
-        <Text style={styles.grayText}>try me</Text>
-      </View>
-    </ScrollView>
-  );
+    return (
+        <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+            <View style={[styles.rowContainer, styles.titleContainer]}>
+                <Text style={styles.title}>
+                    {i18n.t("U3", { defaultValue: "Full Details" })}
+                </Text>
+            </View>
+            <View style={[styles.rowContainer, styles.rowGray]}>
+                <Text style={styles.text}>
+                    {i18n.t("U4", { defaultValue: "Location Audited" })}
+                </Text>
+            </View>
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("B6", { defaultValue: "Department 4" })} 4
+                </Text>
+            </View>
+            <View style={styles.grayEmpty} />
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("U10", { defaultValue: "Date and Time" })} 4
+                </Text>
+                <Text style={styles.grayText}>{formattedDate}</Text>
+            </View>
+            <View style={styles.grayEmpty} />
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("U11", { defaultValue: "Type of HCW 4" })}
+                </Text>
+                <Text style={styles.grayText}>Nurse</Text>
+            </View>
+            <View style={styles.grayEmpty} />
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("ADD2", { defaultValue: "Moments" })}
+                </Text>
+                <Text style={styles.grayText}>1 2 3</Text>
+            </View>
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("U14", { defaultValue: "H.H. Action" })}
+                </Text>
+                <Text style={styles.grayText}>
+                    {i18n.t("AE10", { defaultValue: "Wash" })}
+                </Text>
+            </View>
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("U15", { defaultValue: "H.H. Result" })}
+                </Text>
+                <Text style={styles.grayText}>
+                    {i18n.t("S10", { defaultValue: "Passed" })}
+                </Text>
+            </View>
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("U16", { defaultValue: "Occupational Risk" })}
+                </Text>
+                <Text style={styles.grayText}>-</Text>
+            </View>
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("U17", { defaultValue: "Glove Compliance" })}
+                </Text>
+                <Text style={styles.grayText}>-</Text>
+            </View>
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("U18", { defaultValue: "Gown Compliance" })}
+                </Text>
+                <Text style={styles.grayText}>-</Text>
+            </View>
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("U19", { defaultValue: "Mask Compliance" })}
+                </Text>
+                <Text style={styles.grayText}>-</Text>
+            </View>
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("U20", { defaultValue: "Mask Type" })}
+                </Text>
+                <Text style={styles.grayText}>-</Text>
+            </View>
+            <View style={styles.rowContainer}>
+                <Text style={styles.text}>
+                    {i18n.t("U21", { defaultValue: "Notes" })}
+                </Text>
+                <Text style={styles.grayText}>try me</Text>
+            </View>
+        </ScrollView>
+    );
 };
 
 const styles = createStyleSheet({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-  rowContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: 44,
-    paddingHorizontal: 15,
-    borderBottomWidth: 0.4,
-    borderBottomColor: colors.lightGray,
-  },
-  titleContainer: {
-    justifyContent: "center",
-    height: 40,
-  },
-  title: {
-    textAlign: "center",
-    fontSize: 14,
-    color: colors.midNight,
-  },
-  text: {
-    color: colors.midNight,
-    fontSize: 14,
-  },
-  rowGray: {
-    backgroundColor: "#fafafa",
-  },
-  grayText: {
-    color: colors.cadetGrey,
-  },
-  grayEmpty: {
-    height: 35,
-    borderBottomWidth: 0.4,
-    borderBottomColor: colors.lightGray,
-    backgroundColor: "#fafafa",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+    },
+    rowContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: 44,
+        paddingHorizontal: 15,
+        borderBottomWidth: 0.4,
+        borderBottomColor: colors.lightGray,
+    },
+    titleContainer: {
+        justifyContent: "center",
+        height: 40,
+    },
+    title: {
+        textAlign: "center",
+        fontSize: 14,
+        color: colors.midNight,
+    },
+    text: {
+        color: colors.midNight,
+        fontSize: 14,
+    },
+    rowGray: {
+        backgroundColor: "#fafafa",
+    },
+    grayText: {
+        color: colors.cadetGrey,
+    },
+    grayEmpty: {
+        height: 35,
+        borderBottomWidth: 0.4,
+        borderBottomColor: colors.lightGray,
+        backgroundColor: "#fafafa",
+    },
 });
 
 export default Reports;
